cdb91ce8678ff1f13f6f02861610e176680077eb
Fysal Beauferris <fysal.beauferris1@ucalgary.ca>
2022-09-23 00:58:43 -0600

    added touch events

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
+const btn1 = document.getElementById("btn1")
+const btn2 = document.getElementById("btn2")
+const ropeKnot = document.getElementById("ropeKnot")
 
-$(document).ready(function() {
-    // $.fn.randomMoveFunc = function(idTag, propertyName){
-    //   var string = $(idTag).css(propertyName)
-    //   var sub = string.substring(0,string.length-2); 
-    //   let x = Math.floor((Math.random() * 100) + 1);
-    //   return sub;
-    // }
+btn1.addEventListener("touchstart" , e => {
+  e.preventDefault();
+  // console.log("Btn1-Touches" , e.touches.length)
+  // console.log("Btn1-Targets" , e.targetTouches.length)
+  // console.log("Btn1-Changed" , e.changedTouches.length)
 
-    $("#btn1").click(function() {
-        var string = $("#ropeKnot").css("top")
-        var sub = string.substring(0, string.length-2);
-      $('#ropeKnot').css(
-        "top", ((parseInt(sub) - 10) +"px")
-      );
-    });
-    $("#btn2").click(function() {
-        var string = $("#ropeKnot").css("top")
-        var sub = string.substring(0, string.length-2);
-      $('#ropeKnot').css(
-        "top", ((parseInt(sub) + 10) +"px")
-      );
-      // $("#btn2").css(randomMoveFunc("top"))
-    });
-  });
+  const ropeKnotStyle = window.getComputedStyle(ropeKnot);
+  const ropeKnotTopVal = ropeKnotStyle.getPropertyValue("top")
+  // console.log(ropeKnotTopVal)
+  var str = JSON.stringify(ropeKnotTopVal)
+  // console.log(str)
+  var topSubstring = str.substring(1, str.length-3)
+  // console.log(topSubstring)
+  ropeKnot.style.top  = (parseInt(topSubstring) - 10) +"px"
+  console.log(ropeKnot.style.top)
+
+})
+
+
+btn2.addEventListener("touchstart" , e => {
+  e.preventDefault();
+  // console.log("Btn2-Touches" , e.touches.length)
+  // console.log("Btn2-Targets" , e.targetTouches.length)
+  // console.log("Btn2-Changed" , e.changedTouches.length)
+
+  const ropeKnotStyle = window.getComputedStyle(ropeKnot);
+  const ropeKnotTopVal = ropeKnotStyle.getPropertyValue("top")
+  // console.log(ropeKnotTopVal)
+  var str = JSON.stringify(ropeKnotTopVal)
+  // console.log(str)
+  var topSubstring = str.substring(1, str.length-3)
+  // console.log(topSubstring)
+  ropeKnot.style.top  = (parseInt(topSubstring) + 10) +"px"
+  console.log(ropeKnot.style.top)
+})
+
+document.addEventListener("touchstart", e => {
+  [...e.changedTouches].forEach(touch => {
+    const dot = document.createElement("div")
+    dot.classList.add("dot")
+    dot.style.top = `${touch.pageY}px`
+    dot.style.left = `${touch.pageX}px`
+    dot.id = touch.identifier
+    document.body.append(dot)
+
+
+  })
+})
+
+document.addEventListener("touchmove", e => {
+  [...e.changedTouches].forEach(touch => {
+    const dot = document.getElementById(touch.identifier)
+    dot.style.top = `${touch.pageY}px`
+    dot.style.left = `${touch.pageX}px`
+  })
+})
+
+document.addEventListener("touchend", e => {
+  [...e.changedTouches].forEach(touch => {
+    const dot = document.getElementById(touch.identifier)
+    dot.remove()
+  })
+})
